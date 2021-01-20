931346ab3e4ecabc56ce6731f1ab0f0b8594e920
jasonthealmighty@gmail.com <jasonthealmighty@gmail.com>
2021-01-19 17:09:29 -0700

    nasa

---
+++ src/components/ResultComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="ResultComponent">
+    <button
+      @click="getPicture"
+      class="btn btn-primary"
+    >
+      Click on me to get a picture
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ResultCompontent',
+  setup () {
+    return {
+      getPicture () {
+        pictureService.getPicture()
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
